6775b0b6fb1322ab53c9216478eed7198d057be9
Kyeong Oh Kim <cdkkim08@gmail.com>
2017-03-24 10:43:37 +0900

    refactoring dot-files

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
 ln -s ./dot-files/vimrc ~/.vimrc
 ln -s ./dot-files/tmux.conf ~/.tmux.conf
 ln -s ./dot-files/bashrc ~/.bashrc
-ln -s ./dot-files/vim ~/.vim
+ln -s ./dot-files/bash_profile ~/.bash_profile
+ln -s ./dot-files/inputrc ~/.inputrc
+cp ./dot-files/vim ~/.vim
 
 export DOTFILES=$HOME/dot-files
-files=("$DOTFILES/.path" "$DOTFILES/.export" "$DOTFILES/.aliases" "$DOTFILES/.functions" "$DOTFILES/.extra");
+files=("$DOTFILES/path" "$DOTFILES/export" "$DOTFILES/aliases" "$DOTFILES/functions" "$DOTFILES/extra");
 
 for file in "${files[@]}"; do
   [ -r "$file" ] && [ -f "$file" ] && source "$file";
@@ -12,4 +15,10 @@ done;
 
 unset files file;
 
+# install Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 vim +PluginInstall +qall
+# install YCM
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
